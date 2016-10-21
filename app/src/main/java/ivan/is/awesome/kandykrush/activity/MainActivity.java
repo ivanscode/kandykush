a58bd8339379bd648ffb7fc6c58c32d425935108
ivank <ivan.kouz333@gmail.com>
2016-10-20 18:32:26 -0700

    Restructuring

---
+++ app/src/main/java/ivan/is/awesome/kandykrush/activity/MainActivity.java
@@ -0,0 +1,69 @@
+package ivan.is.awesome.kandykrush.activity;
+
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import ivan.is.awesome.kandykrush.R;
+import ivan.is.awesome.kandykrush.fragments.MainPage;
+import ivan.is.awesome.kandykrush.fragments.Memes;
+
+public class MainActivity extends FragmentActivity {
+    private static final int NUM_PAGES = 2;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new MainPage();
+                case 1:
+                    return new Memes();
+            }
+            return new Memes();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
---
+++ app/src/main/java/ivan/is/awesome/kandykrush/fragments/Memes.java
@@ -0,0 +1,76 @@
+package ivan.is.awesome.kandykrush.fragments;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import ivan.is.awesome.kandykrush.R;
+import ivan.is.awesome.kandykrush.utils.ListAdapter;
+
+public class Memes extends Fragment {
+
+    public MediaPlayer[] mp;
+    ArrayList<String> titles = new ArrayList<>();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.drawer_layout, container, false);
+        ListView mDrawerList = (ListView) rootView.findViewById(R.id.left_drawer);
+        setupMedia();
+        titles.add("stop");
+        mDrawerList.setAdapter(new ListAdapter(getActivity(), mp, titles));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(titles.get(position).equals("stop")){
+                    stopMedia();
+                }else{
+                    mp[position].start();
+                }
+            }
+        });
+        return rootView;
+    }
+    public void stopMedia(){
+        boolean check = false;
+        for(int x=0; x<mp.length; x++){
+            if(mp[x].isPlaying()){
+                check=true;
+            }
+        }
+        if(check) {
+            new Thread(new Runnable() {
+                public void run() {
+                    for (int x = 0; x < mp.length; x++) {
+                        if (mp[x].isPlaying()) {
+                            mp[x].stop();
+                        }
+                        mp[x].release();
+                        mp[x] = null;
+                    }
+                    stopMedia();
+                }
+            }).start();
+        }
+    }
+    public void setupMedia() {
+        Field[] fields = R.raw.class.getFields();
+        // loop for every file in raw folder
+        mp = new MediaPlayer[fields.length];
+        for(int x=0; x < fields.length; x++) {
+            String filename = fields[x].getName();
+            mp[x] = MediaPlayer.create(getActivity(), getResources().getIdentifier(filename, "raw", getActivity().getPackageName()));
+            filename = filename.substring(0, 1).toUpperCase() + filename.substring(1);
+            filename = filename.replace("_", " ");
+            titles.add(filename);
+        }
+    }
+}
+
